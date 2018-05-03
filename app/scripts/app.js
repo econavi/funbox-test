d7ebbc3979a6cda6138d61d5b729f44b5669577e
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-05-04 00:09:26 +0300

    Инициализация проекта

---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+// import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
+// import mainMenu from './../blocks/main-menu/main-menu';
+import product from './../blocks/product/product';
+
+// window.onload = () => {
+// 	svg4everybody();
+// };
